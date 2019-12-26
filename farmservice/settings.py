cf9b70546f431824b9b593a19c2560846435ff97
root <root@162-241-200-194.webhostbox.net>
2019-12-26 19:12:44 +0000

    server change

--- farmservice/settings.py
+++ farmservice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(bz2j+#6zn#svb%aatu2-#!9)y4u)$*snx83lykj6dl!l84)8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['162.241.200.194', ]
+ALLOWED_HOSTS = ['farmket.in', ]
 
 AUTH_USER_MODEL = 'accounts.User'
 
