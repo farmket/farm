746e6c36fdc927f8e71bb2437257e249fec869ee
farmket <reset.farmket@gmail.com>
2020-02-01 13:12:44 +0530

    local changes

--- farmservice/settings.py
+++ farmservice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(bz2j+#6zn#svb%aatu2-#!9)y4u)$*snx83lykj6dl!l84)8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['farmket.in', ]
 
